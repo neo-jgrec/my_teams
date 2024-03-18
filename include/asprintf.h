62489b3f5aea5435abc02d4de152b4567391d3d1
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-03-19 00:49:25 +0100

    feat(general headers): asprintf include

---
+++ include/asprintf.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** my_teams [WSL: Arch]
+** File description:
+** asprintf
+*/
+
+#ifndef ASPRINTF_H_
+    #define ASPRINTF_H_
+
+    #ifndef asprintf
+        #ifndef _GNU_SOURCE
+            #define _GNU_SOURCE
+        #endif
+        #include <stdio.h>
+
+        int asprintf(char **strp, const char *fmt, ...);
+    #endif
+
+#endif /* !ASPRINTF_H_ */
