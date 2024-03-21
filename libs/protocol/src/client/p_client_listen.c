bef23b38d6707b5f2920f4882418de80c10bca9b
LouisLanganay <louislanganay@gmail.com>
2024-03-21 16:55:40 +0100

    fix(Protocol): memory allocation p_client_listen

--- libs/protocol/src/client/p_client_listen.c
+++ libs/protocol/src/client/p_client_listen.c
@@ -6,33 +6,49 @@
 */
 
 #include "../include/protocol.h"
+#include "../../../include/debug_print.h"
+
+static int read_header(int fd, p_payload_t *payload)
+{
+    size_t size = read(fd, &(payload->packet), sizeof(p_packet_t));
+
+    if (size == (size_t)-1) {
+        perror("Header read failed");
+        return -1;
+    }
+    DEBUG_PRINT("Packet header received with id %d and size %d\n",
+        payload->packet.id, payload->packet.size);
+    return 0;
+}
+
+static int read_body(int fd, p_payload_t *payload)
+{
+    size_t size = read(fd, payload->data, payload->packet.size);
+
+    if (size == (size_t)-1) {
+        perror("Payload data read failed");
+        return -1;
+    }
+    DEBUG_PRINT("Packet body received with size %d\n", payload->packet.size);
+    return 0;
+}
 
 p_payload_t* p_client_listen(p_client_t *client) {
-    p_payload_t *payload = (p_payload_t*)malloc(sizeof(p_payload_t));
-    int sockfd = client->network_data.sockfd;
-    p_packet_t packet;
+    p_payload_t* payload = (p_payload_t*)malloc(sizeof(p_payload_t));
 
     if (!payload) {
-        perror("Memory allocation failed");
+        perror("Malloc failed");
         return NULL;
     }
-    if (recv(sockfd, &packet, sizeof(packet), 0) < 0) {
-        perror("Receive failed");
+    if (read_header(client->network_data.sockfd, payload) == -1)
+        return NULL;
+    payload->data = malloc(payload->packet.size);
+    if (payload->data == NULL) {
+        perror("Malloc failed");
         free(payload);
         return NULL;
     }
-    payload->data = malloc(packet.size);
-    if (!payload->data) {
-        perror("Memory allocation failed");
-        free(payload);
+    if (read_body(client->network_data.sockfd, payload) == -1)
         return NULL;
-    }
-    if (recv(sockfd, payload->data, packet.size, 0) < 0) {
-        perror("Receive failed");
-        free(payload->data);
-        free(payload);
-        return NULL;
-    }
-    payload->packet = packet;
     return payload;
 }
