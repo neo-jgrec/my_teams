312755b58b0fb8662f2f78e2f3fdd686e9fd53e4
LouisLanganay <louislanganay@gmail.com>
2024-04-02 17:51:11 +0200

    fix(Protocole): Add server and to test folder

---
+++ tests/client/src/main.c
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2024
+** my_teams
+** File description:
+** main
+*/
+
+#include "../include/client.h"
+#include "../../../libs/protocol/include/protocol.h"
+#include "../../../libs/protocol/include/protocol_client.h"
+#include "../../../libs/protocol/include/protocol_server.h"
+#include "../../../include/debug_print.h"
+
+#define MAX_NAME_LENGTH 32
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/client.h"
+#include "../../../libs/protocol/include/protocol.h"
+#include "../../../libs/protocol/include/protocol_client.h"
+#include "../../../libs/protocol/include/protocol_server.h"
+#include "../../../include/debug_print.h"
+
+#define MAX_NAME_LENGTH 32
+
+int main(void)
+{
+    p_client_t* client = p_client_create("127.0.0.1", 1235);
+    if (!client) {
+        fprintf(stderr, "Client creation failed\n");
+        return 1;
+    }
+
+    p_client_send_packet(EVT_LOGIN, "Hello", 6, client);
+    printf("Sent login packet\n");
+    while (1) {
+        printf("fd %d\n", client->network_data.sockfd);
+        p_payload_t* payload = p_client_listen(client);
+        if (payload) {
+            printf("Received packet of type %d\n", payload->packet.id);
+            printf("Received payload of size %d\n", payload->packet.size);
+            printf("Received payload: %s\n", (char*)payload->data);
+            printf("Received from client %d\n", payload->network_data.sockfd);
+            free(payload->data);
+            free(payload);
+        }
+    }
+
+    close(client->network_data.sockfd);
+    free(client);
+    return 0;
+}
---
+++ tests/server/src/main.c
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2024
+** my_teams [WSL: Arch]
+** File description:
+** main
+*/
+
+#include <stdio.h>
+#include "../../../include/debug_print.h"
+#include "../../../libs/protocol/include/protocol.h"
+#include "../../../libs/protocol/include/protocol_client.h"
+#include "../../../libs/protocol/include/protocol_server.h"
+
+int main(void)
+{
+    printf("Super le server !\n");
+    DEBUG_PRINT("Super debug du %d\n", 42);
+
+    p_server_t* server = p_server_create(1235);
+    if (!server) {
+        fprintf(stderr, "Server creation failed\n");
+        return 1;
+    }
+
+    p_payload_t *payload_resp = p_create_payload(
+        EVT_SUCCES, "Tamere\n", 8
+    );
+
+    while (1) {
+        p_payload_t* payload = p_server_listen(server);
+        if (payload) {
+            printf("Received packet of type %d\n", payload->packet.id);
+            printf("Received payload of size %d\n", payload->packet.size);
+            printf("Received payload: %s\n", (char*)payload->data);
+            printf("Received from client %d\n", payload->network_data.sockfd);
+            if (payload->packet.id == EVT_LOGIN)
+                p_server_send_packet(payload_resp, payload->network_data.sockfd, server);
+            free(payload->data);
+            free(payload);
+        }
+    }
+
+    close(server->network_data.sockfd);
+    free(server);
+
+    return 0;
+}
