1b9d3ba0bf5dd45dffac14594224f764ebd527b8
LouisLanganay <louislanganay@gmail.com>
2024-04-09 20:39:51 +0200

    feat(Protocol): Add new events to the server_event_t enum in protocol_server.h

--- libs/protocol/include/protocol_server.h
+++ libs/protocol/include/protocol_server.h
@@ -10,7 +10,15 @@
 
 typedef enum server_event_e {
     EVT_ERROR,
-    EVT_SUCCES
+    EVT_SUCCESS,
+    EVT_INFO_USER,
+    EVT_INFO_TEAM,
+    EVT_INFO_CHANNEL,
+    EVT_INFO_THREAD,
+    EVT_INFO_REPLY,
+    EVT_INFO_MESSAGE,
+    EVT_INFO_SUBSCRIPTION,
+    EVT_INFO_UNSUBSCRIBE
 } server_event_t;
 
 #endif /* !PROTOCOL_SERVER_H_ */
