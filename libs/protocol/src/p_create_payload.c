e51c7702087a21811a273e7185fcb64b59081451
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-04-15 05:38:40 +0200

    refactor(p_create_payload): add robustness (#10)

--- libs/protocol/src/p_create_payload.c
+++ libs/protocol/src/p_create_payload.c
@@ -13,10 +13,20 @@ p_payload_t *p_create_payload(
 )
 {
     p_payload_t *payload = calloc(1, sizeof(p_payload_t));
+    uint16_t data_size = 0;
 
     if (!payload)
         return NULL;
     payload->packet_type = packet_type;
-    memcpy(payload->data, payload_data, DATA_SIZE);
+    if (payload_data) {
+        data_size = strlen(payload_data) + 1;
+        if (data_size <= DATA_SIZE) {
+            memcpy(payload->data, payload_data, data_size);
+        } else {
+            free(payload);
+            return NULL;
+        }
+    }
+
     return payload;
 }
