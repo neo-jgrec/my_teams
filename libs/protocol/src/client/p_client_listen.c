bd1c65c1bc343de025bb02ac9350f13bc125796b
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-04-15 02:45:07 +0200

    fix(p_client): make the read non-blocking (#8)

--- libs/protocol/src/client/p_client_listen.c
+++ libs/protocol/src/client/p_client_listen.c
@@ -6,15 +6,19 @@
 */
 
 #include "protocol.h"
+#include <fcntl.h>
 
 p_payload_t *p_client_listen(const p_client_t *client)
 {
     p_payload_t *payload = calloc(1, sizeof(p_payload_t));
     const int fd = client->network_data.sockfd;
 
-    if (payload && read(fd, &payload->packet_type, sizeof(uint16_t))
-        && read(fd, payload->data, DATA_SIZE))
-        return payload;
+    if (payload) {
+        fcntl(fd, F_SETFL, O_NONBLOCK);
+        if (read(fd, &payload->packet_type, sizeof(uint16_t))
+            && read(fd, payload->data, DATA_SIZE))
+            return payload;
+    }
     free(payload);
     return NULL;
 }
