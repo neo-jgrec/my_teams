8ddcd0dbcc2ec55a60e4dfb55e576b9d57f89393
Romain <114656699+Neo-Diamons@users.noreply.github.com>
2024-04-17 15:33:56 +0200

    feat(select): use select. (#17)

--- libs/protocol/src/client/p_client_listen.c
+++ libs/protocol/src/client/p_client_listen.c
@@ -6,12 +6,21 @@
 */
 
 #include <unistd.h>
+#include <string.h>
 #include <fcntl.h>
 
 #include "protocol.h"
 
-bool p_client_listen(const p_client_t *client, p_packet_t *packet)
+bool p_client_listen(p_client_t *client, p_packet_t *packet)
 {
-    fcntl(client->network_data.sockfd, F_SETFL, O_NONBLOCK);
+    struct timeval tv = {0, 0};
+
+    memcpy(&client->readfds, &client->master_read_fds, sizeof(fd_set));
+    memcpy(&client->writefds, &client->master_write_fds, sizeof(fd_set));
+    if (select(FD_SETSIZE, &client->readfds, &client->writefds, NULL, &tv)
+        == -1)
+        return false;
+    if (!FD_ISSET(client->network_data.sockfd, &client->readfds))
+        return false;
     return read(client->network_data.sockfd, packet, sizeof(p_packet_t)) > 0;
 }
