465071de114977bfa26a2bd1d8e92232515faaf0
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-04-20 18:41:38 +0200

    fix(unsubscribe): send use

--- client/src/commands/unsubscribe.c
+++ client/src/commands/unsubscribe.c
@@ -8,6 +8,7 @@
 #include "client.h"
 #include "commands.h"
 #include "events.h"
+#include "events_structures.h"
 #include "protocol.h"
 #include "unused.h"
 
@@ -18,16 +19,20 @@ void cmd_unsubscribe(char **args, void *data, UNUSED p_packet_t *packet)
 {
     size_t nb_args = 0;
     p_client_t *p_client = ((c_client_t *)data)->p_client;
+    unsubscribe_t unsubscribe = {0};
 
     for (; args[nb_args]; nb_args++);
     if (nb_args != 2) {
         fprintf(stdout, "Invalid command\n");
         return;
     }
+    strncpy(unsubscribe.team_uuid, args[1], strlen(args[1]));
+    strncpy(unsubscribe.user_uuid, ((c_client_t *)data)->user.uuid,
+        strlen(((c_client_t *)data)->user.uuid));
     p_client_send_packet(
         p_client,
         EVT_UNSUBSCRIBE,
-        args[1],
-        strlen(args[1])
+        &unsubscribe,
+        sizeof(unsubscribe_t)
     );
 }
